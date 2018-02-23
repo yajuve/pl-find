ee918997088b639aef9a2f333de63d41b5d11bfd
MRaouf <yajuve.25.dz@gmail.com>
2018-02-23 15:49:40 +0100

    Add read file and regex

--- pl-find.pl
+++ pl-find.pl
@@ -7,3 +7,11 @@ my $path = $ARGV[0];
 my $word = $ARGV[1];
 
 open FILE, '<:encoding(UTF-8)', $path or die "Error $path : $!";
+
+while (<FILE>) {
+	$_ =~ s/$word/\033[1;32m$word\033[0m/ig if /$word/i; 
+	print "$. - $_" if /$word/i;
+}
+
+close FILE;
+
